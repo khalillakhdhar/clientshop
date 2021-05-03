0877947ab86fe1b5e2edf89de5f8be5d146f463f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-03 13:56:45 +0100

    ignores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:
+  {
+    apiKey: "AIzaSyBFkTjMoUdF7S9ep3Gx_bPdkuhSP5PZiS0",
+    authDomain: "shopaveugle-7e9cd.firebaseapp.com",
+    projectId: "shopaveugle-7e9cd",
+    storageBucket: "shopaveugle-7e9cd.appspot.com",
+    messagingSenderId: "813783582996",
+    appId: "1:813783582996:web:de8248b778dfe40a585a01",
+    measurementId: "G-KSE73Q55CF"
+  }
 };
